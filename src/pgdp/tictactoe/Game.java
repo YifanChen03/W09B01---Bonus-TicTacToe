2a3612443563721b32b3c9341097c9122e222e22
ETH <cyanfhine@gmail.com>
2022-12-27 03:31:08 +0100

    basic game implemented

--- src/pgdp/tictactoe/Game.java
+++ src/pgdp/tictactoe/Game.java
@@ -2,8 +2,12 @@ package pgdp.tictactoe;
 
 import pgdp.tictactoe.ai.HumanPlayer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Game {
@@ -14,6 +18,8 @@ public class Game {
     private boolean[] firstPlayedPieces;
     private boolean[] secondPlayedPieces;
     private PenguAI winner;
+    private List<int[]> fP_fields;
+    private List<int[]> sP_fields;
 
     public Game(PenguAI first, PenguAI second) {
         this.first = first;
@@ -27,6 +33,8 @@ public class Game {
         Arrays.fill(firstPlayedPieces, false);
         Arrays.fill(secondPlayedPieces, false);
         winner = null;
+        fP_fields = new ArrayList<>();
+        sP_fields = new ArrayList<>();
     }
 
     public PenguAI getWinner() {
@@ -35,22 +43,93 @@ public class Game {
 
     public void playGame() {
         Move m;
-        boolean[] currPlayedPieces;
         int x;
         int y;
         int value;
+
         //as long as one of the players has pieces left
         while (checkIfPiecesLeft()) {
             if (firstPlayer) {
                 m = first.makeMove(board, true, firstPlayedPieces, secondPlayedPieces);
+                x = m.x();
+                y = m.y();
+                value = m.value();
+
+                //check if input position was outside of board and if player still has chosen piece
+                if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && !firstPlayedPieces[value]) {
+                    int[] toAdd = new int[]{x, y};
+                    //check if input position is already occupied if yes then check if value is smaller than chosen piece
+                    if (board[x][y] == null) {
+                        board[x][y] = new Field(value, firstPlayer);
+                        firstPlayedPieces[value] = true;
+                        fP_fields.add(toAdd);
+                    } else {
+                        if (board[x][y].value() < value) {
+                            board[x][y] = new Field(value, firstPlayer);
+                            firstPlayedPieces[value] = true;
+                            fP_fields.add(toAdd);
+                            sP_fields.removeIf(lE -> lE.equals(toAdd));
+                        } else {
+                            illegalMove();
+                            break;
+                        }
+                    }
+                } else {
+                    illegalMove();
+                    break;
+                }
+            } else {
+                m = second.makeMove(board, true, firstPlayedPieces, secondPlayedPieces);
+                x = m.x();
+                y = m.y();
+                value = m.value();
+
+                //check if input position was outside of board and if player still has chosen piece
+                if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && !secondPlayedPieces[value]) {
+                    int[] toAdd = new int[]{x, y};
+                    //check if input position is already occupied if yes then check if value is smaller than chosen piece
+                    if (board[x][y] == null) {
+                        board[x][y] = new Field(value, firstPlayer);
+                        secondPlayedPieces[value] = true;
+                        sP_fields.add(toAdd);
+                    } else {
+                        if (board[x][y].value() < value) {
+                            board[x][y] = new Field(value, firstPlayer);
+                            secondPlayedPieces[value] = true;
+                            sP_fields.add(toAdd);
+                            fP_fields.removeIf(lE -> lE.equals(toAdd));
+                        } else {
+                            illegalMove();
+                            break;
+                        }
+                    }
+                } else {
+                    illegalMove();
+                    break;
+                }
+            }
+            if (checkWin() != null)
+                break;
+            firstPlayer = !firstPlayer;
+        }
+        //Move m;
+        /*boolean[] currPlayedPieces;
+        //int x;
+        //int y;
+        //int value;
+
+        while (checkIfPiecesLeft()) {
+            if (firstPlayer) {
+                //m = first.makeMove(board, true, firstPlayedPieces, secondPlayedPieces);
                 currPlayedPieces = firstPlayedPieces;
             } else {
                 m = second.makeMove(board, false, firstPlayedPieces, secondPlayedPieces);
                 currPlayedPieces = secondPlayedPieces;
             }
-            x = m.x();
-            y = m.y();
-            value = m.value();
+            //x = m.x();
+            //y = m.y();
+            //value = m.value();
+
             //check if input position was outside of board and if player still has chosen piece
             if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && !currPlayedPieces[value]) {
                 //check if input position is already occupied if yes then check if value is smaller than chosen piece
@@ -66,19 +145,21 @@ public class Game {
                         break;
                     }
                 }
-                printBoard(board);
             } else {
                 illegalMove();
                 break;
             }
             if (firstPlayer) {
                  firstPlayedPieces = currPlayedPieces;
+                 fP_fields.add(new int[]{x, y});
             } else {
                 secondPlayedPieces = currPlayedPieces;
+                sP_fields.add(new int[]{x, y});
             }
+            if (checkWin() != null)
+                return;
             firstPlayer = !firstPlayer;
-            printBoard(board);
-        }
+        }*/
     }
 
     public static void printBoard(Field[][] board) {
@@ -120,6 +201,38 @@ public class Game {
         } else {
             winner = first;
         }
+    }
+
+    public PenguAI checkWin() {
+        List<List<int[]>> winningPlaces = new ArrayList<>();
+
+        List<int[]> firstRow = Arrays.asList(new int[][]{{0, 0}, {1, 0}, {2, 0}});
+        List<int[]> secondRow = Arrays.asList(new int[][]{{0, 1}, {1, 1}, {2, 1}});
+        List<int[]> thirdRow = Arrays.asList(new int[][]{{0, 2}, {1, 2}, {2, 2}});
+        List<int[]> firstCol = Arrays.asList(new int[][]{{0, 0}, {0, 1}, {0, 2}});
+        List<int[]> secondCol = Arrays.asList(new int[][]{{1, 0}, {1, 1}, {1, 2}});
+        List<int[]> thirdCol = Arrays.asList(new int[][]{{2, 0}, {2, 1}, {2, 2}});
+        List<int[]> firstDia = Arrays.asList(new int[][]{{0, 0}, {1, 1}, {2, 2}});
+        List<int[]> secondDia = Arrays.asList(new int[][]{{0, 2}, {1, 1}, {2, 0}});
+
+        winningPlaces.add(firstRow);
+        winningPlaces.add(secondRow);
+        winningPlaces.add(thirdRow);
+        winningPlaces.add(firstCol);
+        winningPlaces.add(secondCol);
+        winningPlaces.add(thirdCol);
+        winningPlaces.add(firstDia);
+        winningPlaces.add(secondDia);
+
+        for (List<int[]> row : winningPlaces) {
+            if (fP_fields.stream().map(le -> Arrays.toString(le)).collect(Collectors.toList())
+                    .containsAll(row.stream().map(le -> Arrays.toString(le)).collect(Collectors.toList())))
+                return first;
+            if (sP_fields.stream().map(le -> Arrays.toString(le)).collect(Collectors.toList())
+                    .containsAll(row.stream().map(le -> Arrays.toString(le)).collect(Collectors.toList())))
+                return second;
+        }
+        return null;
     }
 
     public static void main(String[] args) {
