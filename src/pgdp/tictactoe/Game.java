38ff65315ae84898e47b08f906aae2e8324dee39
ETH <cyanfhine@gmail.com>
2022-12-27 03:58:52 +0100

    basic game implemented

--- src/pgdp/tictactoe/Game.java
+++ src/pgdp/tictactoe/Game.java
@@ -56,7 +56,8 @@ public class Game {
                 value = m.value();
 
                 //check if input position was outside of board and if player still has chosen piece
-                if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && !firstPlayedPieces[value]) {
+                if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && value < 9 && value >= 0
+                        && !firstPlayedPieces[value]) {
                     int[] toAdd = new int[]{x, y};
                     //check if input position is already occupied if yes then check if value is smaller than chosen piece
                     if (board[x][y] == null) {
@@ -65,13 +66,13 @@ public class Game {
                         fP_fields.add(toAdd);
                     } else {
                         if (board[x][y].value() < value) {
-                            board[x][y] = new Field(value, firstPlayer);
-                            firstPlayedPieces[value] = true;
-                            fP_fields.add(toAdd);
-                            sP_fields.removeIf(lE -> lE.equals(toAdd));
-                        } else {
-                            illegalMove();
-                            break;
+                                board[x][y] = new Field(value, firstPlayer);
+                                firstPlayedPieces[value] = true;
+                                fP_fields.add(toAdd);
+                                sP_fields.removeIf(lE -> Arrays.toString(lE).equals(Arrays.toString(toAdd)));
+                            } else {
+                                illegalMove();
+                                break;
                         }
                     }
                 } else {
@@ -85,7 +86,8 @@ public class Game {
                 value = m.value();
 
                 //check if input position was outside of board and if player still has chosen piece
-                if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && !secondPlayedPieces[value]) {
+                if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && value < 9 && value >= 0
+                        && !secondPlayedPieces[value]) {
                     int[] toAdd = new int[]{x, y};
                     //check if input position is already occupied if yes then check if value is smaller than chosen piece
                     if (board[x][y] == null) {
@@ -97,7 +99,7 @@ public class Game {
                             board[x][y] = new Field(value, firstPlayer);
                             secondPlayedPieces[value] = true;
                             sP_fields.add(toAdd);
-                            fP_fields.removeIf(lE -> lE.equals(toAdd));
+                            fP_fields.removeIf(lE -> Arrays.toString(lE).equals(Arrays.toString(toAdd)));
                         } else {
                             illegalMove();
                             break;
@@ -115,54 +117,6 @@ public class Game {
             }
             firstPlayer = !firstPlayer;
         }
-        //Move m;
-        /*boolean[] currPlayedPieces;
-        //int x;
-        //int y;
-        //int value;
-
-        while (checkIfPiecesLeft()) {
-            if (firstPlayer) {
-                //m = first.makeMove(board, true, firstPlayedPieces, secondPlayedPieces);
-                currPlayedPieces = firstPlayedPieces;
-            } else {
-                m = second.makeMove(board, false, firstPlayedPieces, secondPlayedPieces);
-                currPlayedPieces = secondPlayedPieces;
-            }
-            //x = m.x();
-            //y = m.y();
-            //value = m.value();
-
-            //check if input position was outside of board and if player still has chosen piece
-            if (x <= 2 && x >= 0 && y <= 2 && y >= 0 && !currPlayedPieces[value]) {
-                //check if input position is already occupied if yes then check if value is smaller than chosen piece
-                if (board[x][y] == null) {
-                    board[x][y] = new Field(value, firstPlayer);
-                    currPlayedPieces[value] = true;
-                } else {
-                    if (board[x][y].value() < value) {
-                        board[x][y] = new Field(value, firstPlayer);
-                        currPlayedPieces[value] = true;
-                    } else {
-                        illegalMove();
-                        break;
-                    }
-                }
-            } else {
-                illegalMove();
-                break;
-            }
-            if (firstPlayer) {
-                 firstPlayedPieces = currPlayedPieces;
-                 fP_fields.add(new int[]{x, y});
-            } else {
-                secondPlayedPieces = currPlayedPieces;
-                sP_fields.add(new int[]{x, y});
-            }
-            if (checkWin() != null)
-                return;
-            firstPlayer = !firstPlayer;
-        }*/
     }
 
     public static void printBoard(Field[][] board) {
